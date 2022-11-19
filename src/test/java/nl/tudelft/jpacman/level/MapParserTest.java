125e3721a757f10359e713989673a768e0c95a91
Chaiyasit Apirakkitikul <tempest545454@gmail.com>
2022-11-19 17:02:11 +0700

    Working on JPacman Exercise5 Task 13

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,7 +1,9 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -48,5 +50,31 @@ public class MapParserTest {
         Mockito.verify(boardFactory, Mockito.times(createGroundCount)).createGround();
         Mockito.verify(boardFactory, Mockito.times(createWallCount)).createWall();
     }
+
+    /**
+     * Test for the parseMap method (bad map).
+     */
+    @Test
+    public void testParseMapWrong1() {
+        PacmanConfigurationException thrown = Assertions.assertThrows(PacmanConfigurationException.class, () -> {
+            MockitoAnnotations.initMocks(this);
+            assertNotNull(boardFactory);
+            assertNotNull(levelFactory);
+            MapParser mapParser = new MapParser(levelFactory, boardFactory);
+            ArrayList<String> map = new ArrayList<>();
+                /*
+                Create a map with inconsistent size between
+                each row or contain invalid characters
+                */
+            map.add("############");
+            map.add("#P   B    G#");
+            map.add("############");
+
+            mapParser.parseMap(map);
+        });
+        Assertions.assertEquals("Invalid character at 5,1: B", thrown.getMessage());
+    }
+
+
 
 }
